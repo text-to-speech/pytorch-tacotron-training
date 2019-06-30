277d752f8a0cbb17cd0dd2b5c79ce6ca2b77f01c
Leggera <lana.krlv1995@gmail.com>
2019-06-30 22:31:11 +0300

    Update Small_wavs_for_pytorch_tacotron2.sh

--- Small_wavs_for_pytorch_tacotron2.sh
+++ Small_wavs_for_pytorch_tacotron2.sh
@@ -1,12 +1,12 @@
 git clone https://github.com/text-to-speech/tacotron2.git
 pip install torch torchvision
 git clone https://github.com/NVIDIA/apex
-cd 'apex/'
+cd apex/
 pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" .
-cd '../'tacotron2/'
+cd ../'tacotron2/
 git submodule init; git submodule update
 pip install -r requirements.txt
-py_dir=data/pytorch-Tacotron/
+py_dir=/content/gdrive/My\ Drive/Research/data/pytorch-Tacotron/
 outdir='${py_dir}output/'
 logdir='${py_dir}log/'
 c_path='${outdir}checkpoint_129000'
